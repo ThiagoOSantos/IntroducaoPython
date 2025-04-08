e6cdf038b1419d0f4e3f22baa3748a2dde2887a2
Thiago de Oliveira <45471767+ThiagoOSantos@users.noreply.github.com>
2025-04-08 17:49:37 -0300

    Add files via upload

---
+++ Python-Thiago/aula01.py
@@ -0,0 +1,41 @@
+'''
+inteiros = int
+palavras/letras = string
+fracionado/ponto flutuante = float
+'''
+def bomdia():
+    print("Ola world")
+
+'''bomdia()
+'''
+def saudacao(nome):
+    print(f"Ola, {nome}! Seja bem-vindo ao mundo do Python")
+
+a="Thiago, o fenomenal"
+saudacao(a)
+
+def soma(a,b):
+    resultado=a+b
+    print("O resultado da soma é:", resultado)
+
+def somasimples():
+    soma=2+2
+    print("Essa é a soma de 2+2:", soma)
+
+def div(num,num2):
+    resultado=num/num2
+    print("O resultado da divisao é:", resultado)
+
+def mult(num,num2):
+    resultado=num*num2
+    print("O resultado da multiplicação é:", resultado)
+
+def sub(num,num2):
+    resultado= num-num2
+    print("O resultado da subtração é:", resultado)
+
+div(50,25)
+soma(50,25)
+somasimples()
+mult(50,25)
+sub(50,25)
