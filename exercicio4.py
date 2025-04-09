4cb8609a1899e7a866d3b5aedfb79f6cb7cc1025
Thiago de Oliveira <45471767+ThiagoOSantos@users.noreply.github.com>
2025-04-09 17:52:14 -0300

    Delete exercicio4.py

--- exercicio4.py
+++
@@ -1,8 +0,0 @@
-def horastrabalhadas(hora, valorhora):
-    horas = float(input("Digite a quantidade de horas trabalhadas"))
-    if horas > 8:
-        print("Hora extra registrada")
-    valorhoras = float(input("Digite o valor da hora trabalhada"))
-    print(f"Esse é o total de {horas} trabalhadas: {valorhoras}")
-    
-    horastrabalhadas()
