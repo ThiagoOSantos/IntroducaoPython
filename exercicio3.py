6ff6ef61209b3dbaa22a9fa5f3392d644a11b032
Thiago de Oliveira <45471767+ThiagoOSantos@users.noreply.github.com>
2025-04-09 17:52:06 -0300

    Delete exercicio3.py

--- exercicio3.py
+++
@@ -1,9 +0,0 @@
-def nota():
-    num = float(input("Digite o valor da nota do aluno"))
-    if num >=7:
-        print("Aluno aprovado")
-    elif num >= 5 and num <= 6.9:
-        print("Aluno em recuperação")
-    else:
-        print("Aluno reprovado")
-nota()
