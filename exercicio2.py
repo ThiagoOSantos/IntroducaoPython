7186d8d38fae14bd67bc2180c0dac807f4ef82bf
Thiago de Oliveira <45471767+ThiagoOSantos@users.noreply.github.com>
2025-04-09 17:51:58 -0300

    Delete exercicio2.py

--- exercicio2.py
+++
@@ -1,9 +0,0 @@
-def desconto():
-    valor = float(input("Digite o valor do item para desconto"))
-    if valor>100:
-        valor = valor - valor/10
-        print(f"o valor do item com o desconto é de {valor}")
-    else:
-        print("Não é possível aplicar o desconto da promoção")
-
-desconto()
